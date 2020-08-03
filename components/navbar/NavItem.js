def994e9c83dfda4aac50b5eddce21274ead76b7
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-08-03 19:11:21 +0900

    test

---
+++ components/navbar/NavItem.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const box = styled.div``;
+
+const Index = (props) => {
+  return <div></div>;
+};
+
+export default Index;
